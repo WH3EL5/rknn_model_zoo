8307a404725f445c7e2764c862ff5392ab3b90b6
WH3EL5 <wh20160134@outlook.com>
2026-03-24 14:46:34 +0800

    rknn yolov5: add inference time

--- examples/yolov5/cpp/rknpu1/yolov5.cc
+++ examples/yolov5/cpp/rknpu1/yolov5.cc
@@ -155,6 +155,11 @@ int release_yolov5_model(rknn_app_context_t *app_ctx)
     return 0;
 }
 
+longlong __get_us(struct timeval *time)
+{
+    return ((long long)time->tv_sec * 1000000LL + time->tv_usec);
+}
+
 int inference_yolov5_model(rknn_app_context_t *app_ctx, image_buffer_t *img, object_detect_result_list *od_results)
 {
     int ret;
@@ -165,6 +170,7 @@ int inference_yolov5_model(rknn_app_context_t *app_ctx, image_buffer_t *img, obj
     const float nms_threshold = NMS_THRESH;      // Default NMS threshold
     const float box_conf_threshold = BOX_THRESH; // Default box threshold
     int bg_color = 114;
+    struct timeval start_time, end_time;
 
     if ((!app_ctx) || !(img) || (!od_results))
     {
@@ -205,6 +211,7 @@ int inference_yolov5_model(rknn_app_context_t *app_ctx, image_buffer_t *img, obj
     inputs[0].buf = dst_img.virt_addr;
     // inputs[0].buf = img->virt_addr;
 
+    gettimeofday(&start_time, NULL);
     ret = rknn_inputs_set(app_ctx->rknn_ctx, app_ctx->io_num.n_input, inputs);
     if (ret < 0)
     {
@@ -213,7 +220,7 @@ int inference_yolov5_model(rknn_app_context_t *app_ctx, image_buffer_t *img, obj
     }
 
     // Run
-    printf("rknn_run\n");
+    printf("First rknn_run\n");
     ret = rknn_run(app_ctx->rknn_ctx, nullptr);
     if (ret < 0)
     {
@@ -234,6 +241,24 @@ int inference_yolov5_model(rknn_app_context_t *app_ctx, image_buffer_t *img, obj
         printf("rknn_outputs_get fail! ret=%d\n", ret);
         goto out;
     }
+    gettimeofday(&end_time, NULL);
+    long long time_us = __get_us(&end_time) - __get_us(&start_time);
+    printf("First Inference time: %lld us\n", time_us);
+
+    printf("Inference 10 times for more stable performance testing...\n");
+    gettimeofday(&start_time, NULL);
+    for (int i = 0; i < 10; i++)
+    {
+        rknn_inputs_set(app_ctx->rknn_ctx, app_ctx->io_num.n_input, inputs);
+        rknn_run(app_ctx->rknn_ctx, nullptr);
+        rknn_outputs_get(app_ctx->rknn_ctx, app_ctx->io_num.n_output, outputs, NULL);
+        ret = rknn_outputs_release(app_ctx->rknn_ctx, app_ctx->io_num.n_output, outputs);
+    }
+    gettimeofday(&end_time, NULL);
+    printf("Average Inference time: %lld us\n", (__get_us(&end_time) - __get_us(&start_time)) / 10);
+    rknn_perf_detail perf_detail;
+    ret = rknn_query(app_ctx->rknn_ctx, RKNN_QUERY_PERF_DETAIL, &perf_detail, sizeof(perf_detail));
+    printf("%s", perf_detail.perf_data);
 
     // Post Process
     post_process(app_ctx, outputs, &letter_box, box_conf_threshold, nms_threshold, od_results);
