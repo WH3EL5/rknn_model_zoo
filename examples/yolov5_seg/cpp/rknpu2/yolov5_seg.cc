76f090edf461fa8565a0ef618c889cfbdf34eb21
WH3EL5 <wh20160134@outlook.com>
2026-03-25 11:38:41 +0800

    rknn yolo perf

--- examples/yolov5_seg/cpp/rknpu2/yolov5_seg.cc
+++ examples/yolov5_seg/cpp/rknpu2/yolov5_seg.cc
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <math.h>
+#include <sys/time.h>
 
 #include "yolov5_seg.h"
 #include "common.h"
@@ -33,7 +34,7 @@ int init_yolov5_seg_model(const char *model_path, rknn_app_context_t *app_ctx)
         return -1;
     }
 
-    ret = rknn_init(&ctx, model, model_len, 0, NULL);
+    ret = rknn_init(&ctx, model, model_len, RKNN_FLAG_COLLECT_PERF_MASK, NULL);
     free(model);
     if (ret < 0)
     {
@@ -141,6 +142,11 @@ int release_yolov5_seg_model(rknn_app_context_t *app_ctx)
     return 0;
 }
 
+long long __get_us(struct timeval *time)
+{
+    return time->tv_sec * 1000000 + time->tv_usec;
+}
+
 int inference_yolov5_seg_model(rknn_app_context_t *app_ctx, image_buffer_t *img, object_detect_result_list *od_results)
 {
     int ret;
@@ -151,6 +157,10 @@ int inference_yolov5_seg_model(rknn_app_context_t *app_ctx, image_buffer_t *img,
     const float nms_threshold = NMS_THRESH;
     const float box_conf_threshold = BOX_THRESH;
     int bg_color = 114; // pad color for letterbox
+    struct timeval start_time, end_time;
+    long long time_us;
+    rknn_perf_detail perf_detail;
+
 
     if ((!app_ctx) || !(img) || (!od_results))
     {
@@ -200,14 +210,14 @@ int inference_yolov5_seg_model(rknn_app_context_t *app_ctx, image_buffer_t *img,
     }
 
     // Run
-    printf("rknn_run\n");
+    printf("First rknn_run\n");
+    gettimeofday(&start_time, NULL);
     ret = rknn_run(app_ctx->rknn_ctx, nullptr);
     if (ret < 0)
     {
         printf("rknn_run fail! ret=%d\n", ret);
         goto out;
     }
-
     // Get Output
     memset(outputs, 0, sizeof(outputs));
     for (int i = 0; i < app_ctx->io_num.n_output; i++)
@@ -221,6 +231,25 @@ int inference_yolov5_seg_model(rknn_app_context_t *app_ctx, image_buffer_t *img,
         printf("rknn_outputs_get fail! ret=%d\n", ret);
         goto out;
     }
+
+    gettimeofday(&end_time, NULL);
+    time_us = __get_us(&end_time) - __get_us(&start_time);
+    printf("First Inference time: %lld us\n", time_us);
+
+    printf("Inference 10 times for more stable performance testing...\n");
+    gettimeofday(&start_time, NULL);
+    for (int i = 0; i < 10; i++)
+    {
+        rknn_inputs_set(app_ctx->rknn_ctx, app_ctx->io_num.n_input, inputs);
+        rknn_run(app_ctx->rknn_ctx, nullptr);
+        rknn_outputs_get(app_ctx->rknn_ctx, app_ctx->io_num.n_output, outputs, NULL);
+        ret = rknn_outputs_release(app_ctx->rknn_ctx, app_ctx->io_num.n_output, outputs);
+    }
+    gettimeofday(&end_time, NULL);
+    printf("Average Inference time: %lld us\n", (__get_us(&end_time) - __get_us(&start_time)) / 10);
+
+    ret = rknn_query(app_ctx->rknn_ctx, RKNN_QUERY_PERF_DETAIL, &perf_detail, sizeof(perf_detail));
+    printf("%s\n", perf_detail.perf_data);
 
     // Post Process
     post_process(app_ctx, outputs, &letter_box, box_conf_threshold, nms_threshold, od_results);
